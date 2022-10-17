21e7dc684d09341589a5ec8385f19f5ca4dea874
YeonJun Cho <micayel1219@gmail.com>
2022-10-17 10:12:54 +0900

    feat : project setting

---
+++ src/main/java/com/ll/ebook/EBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ll.ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/ebook/EBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
