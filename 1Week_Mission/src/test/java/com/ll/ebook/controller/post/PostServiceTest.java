7b00c57bbfa195c3fcfc952a0737bfc7aeb8d56a
YeonJun Cho <micayel1219@gmail.com>
2022-10-17 14:49:37 +0900

    refactor : 글 생성 beforeeach로 분리

--- 1Week_Mission/src/test/java/com/ll/ebook/controller/post/PostServiceTest.java
+++ 1Week_Mission/src/test/java/com/ll/ebook/controller/post/PostServiceTest.java
@@ -2,6 +2,7 @@ package com.ll.ebook.controller.post;
 
 import com.ll.ebook.post.PostService;
 import com.ll.ebook.post.model.entity.PostEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,12 +25,15 @@ public class PostServiceTest {
     @Autowired
     private PostService postService;
 
+    @BeforeEach
+    void create(){
+        postService.write("user", "subject", "content", "keywords");
+    }
+
     @Test
     @DisplayName("글 작성, 게시글 리스트 확인")
     void getList() throws Exception {
         // WHEN
-        postService.write("user", "subject", "content", "keywords");
-
         //then
         assertThat(postService.findAll().get(0).getId()).isEqualTo(1l);
 
@@ -39,10 +43,8 @@ public class PostServiceTest {
     @DisplayName("글 작성, 수정 후 확인")
     void modify() throws Exception {
         // WHEN
-        postService.write("user", "subject", "content", "keywords");
-
         postService.modify(1l, "new subject", "new content", "new keywords");
-        
+
         //then
         PostEntity postEntity = postService.findById(1l);
 
