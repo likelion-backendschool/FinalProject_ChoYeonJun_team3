d42a1b5e2499b8f4c25643d051754b134a54f1af
YeonJun Cho <micayel1219@gmail.com>
2022-10-17 11:26:18 +0900

    style : controller 메소드 작성 순서 수정

--- 1Week_Mission/src/main/java/com/ll/ebook/post/PostController.java
+++ 1Week_Mission/src/main/java/com/ll/ebook/post/PostController.java
@@ -19,29 +19,6 @@ public class PostController {
     private final PostService postService;
 
     /**
-     * 글 리스트
-     * - 제목과 글의 해시태그들을 볼 수 있다.
-     * - 해시태그를 클릭하면 내가 작성한 글 중 해당 해시태그와 관련된 글들을 볼 수 있다.
-     */
-    @GetMapping("/list")
-    public void getList(Model model){
-
-        List<PostDto> dtos = postService.getList();
-
-        model.addAttribute("posts", dtos);
-    }
-
-    /**
-     * 글 상세
-     * - 글의 제목, 내용, 해시태그를 모두 출력
-     * - 글의 내용은 마크다운 해석이 되어야 한다.
-     */
-    @GetMapping()
-    public void getDetail(@RequestParam Long id){
-
-    }
-
-    /**
      * 글 작성/수정
      * - 마크다운 원문과 렌더링 결과(HTML)까지 같이 저장한다.
      */
@@ -64,6 +41,31 @@ public class PostController {
     public void modify(){
 
     }
+
+    /**
+     * 글 리스트
+     * - 제목과 글의 해시태그들을 볼 수 있다.
+     * - 해시태그를 클릭하면 내가 작성한 글 중 해당 해시태그와 관련된 글들을 볼 수 있다.
+     */
+    @GetMapping("/list")
+    public void getList(Model model){
+
+        List<PostDto> dtos = postService.getList();
+
+        model.addAttribute("posts", dtos);
+    }
+
+    /**
+     * 글 상세
+     * - 글의 제목, 내용, 해시태그를 모두 출력
+     * - 글의 내용은 마크다운 해석이 되어야 한다.
+     */
+    @GetMapping()
+    public void getDetail(@RequestParam Long id){
+
+    }
+
+
 
     /**
      * 글 삭제
