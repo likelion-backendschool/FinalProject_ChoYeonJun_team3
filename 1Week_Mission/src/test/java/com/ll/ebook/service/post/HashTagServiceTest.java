4d0d636e76743813bb960eb4b1ab494de6ca619a
YeonJun Cho <micayel1219@gmail.com>
2022-10-19 10:30:23 +0900

    chore : test 패키지명 miss fix

--- 1Week_Mission/src/test/java/com/ll/ebook/service/post/HashTagServiceTest.java
+++ 1Week_Mission/src/test/java/com/ll/ebook/service/post/HashTagServiceTest.java
@@ -1,5 +1,8 @@
-package com.ll.ebook.controller.post;
+package com.ll.ebook.service.post;
 
+import com.ll.ebook.hashtag.HashTagService;
+import com.ll.ebook.hashtag.model.HashTagEntity;
+import com.ll.ebook.keyword.KeywordService;
 import com.ll.ebook.post.DataNotFoundException;
 import com.ll.ebook.post.PostService;
 import com.ll.ebook.post.model.entity.PostEntity;
@@ -12,35 +15,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 /**
- * 해시태그가 존재 하지 않는 글에 대한 테스트 코드
+ * 해시태그가 존재 하는 글에 대한 테스트 코드
  *
  * ddl-auto 설정
  *   jpa:
  *     hibernate:
  *       ddl-auto: create
  */
-
 @SpringBootTest
-@Transactional
+//@Transactional
 @ActiveProfiles("test")
-public class PostServiceTest {
+public class HashTagServiceTest {
     @Autowired
     private PostService postService;
-
+    @Autowired
+    private HashTagService hashTagService;
+    @Autowired
+    private KeywordService keywordService;
     @Autowired
     private MemberRepository memberRepository;
 
     private final String username = "username";
+    private final String keywords = "#개발 #자바";
 
     /**
      * 매 테스트마다 사용자 새로 생성 및 글 작성
@@ -48,7 +51,7 @@ public class PostServiceTest {
     @BeforeEach
     void create(){
         saveUser();
-        postService.write(username, "subject", "content");
+        postService.write(username, "subject", "content", "#개발 #자바");
     }
 
     /**
@@ -57,7 +60,6 @@ public class PostServiceTest {
      *
      * User 작업 들어가면서 수정 예정
      */
-    @Test
     void saveUser(){
         memberRepository.save(MemberEntity.builder()
                 .email("abc@gmail.com")
@@ -68,35 +70,21 @@ public class PostServiceTest {
     }
 
     @Test
-    @DisplayName("글 작성, 게시글 리스트 확인")
-    void getList() throws Exception {
+    @DisplayName("저장된 해시태그 키워드 확인")
+    void save() throws Exception {
         // WHEN
-        int before = postService.findAll().size();
-        postService.write(username, "subject", "content");
-//        userRepository.save(getUser());
-//        postService.write("username", "subject", "content");
-        //then
-        assertThat(postService.findAll().size()).isGreaterThan(before);
-
-    }
-
-    @Test
-    @DisplayName("글 작성, 수정 후 확인")
-    void modify() throws Exception {
-        // WHEN
-//        postService.write("username", "subject", "content");
-        PostEntity post = postService.findByUserEntity(memberRepository.findByUsername(username).get());
-        postService.modify(post.getId(), "new subject", "new content", "new keywords");
+        PostEntity postEntity = postService.findByUserEntity(memberRepository.findByUsername(username).get());
+        List<HashTagEntity> hashTags = hashTagService.getHashTags(postEntity);
 
         //then
-        PostEntity postEntity = postService.findById(post.getId());
-
-        assertThat(postEntity.getSubject()).isEqualTo("new subject");
-        assertThat(postEntity.getContent()).isEqualTo("new content");
-
+        for(HashTagEntity hashTag : hashTags){
+            assertThat(hashTag.getKeywordEntity().getContent()).isEqualTo(keywordService.findById(hashTag.getKeywordEntity().getId()).getContent());
+        }
     }
+
+
     @Test
-    @DisplayName("글 작성, 삭제 후 확인")
+    @DisplayName("글 작성, 삭제 후 해시태그도 같이 삭제")
     void delete() throws Exception {
         // when
 //        postService.write("username", "subject", "content");
@@ -111,8 +99,6 @@ public class PostServiceTest {
         String expectedMessage = "no %d post not found,".formatted(postEntity.getId());
         String actualMessage = exception.getMessage();
         assertTrue(actualMessage.contains(expectedMessage));
-
-
     }
 
 }
