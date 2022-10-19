774b26121806bf2c3788e67f0085056882dcef09
YeonJun Cho <micayel1219@gmail.com>
2022-10-19 13:18:37 +0900

    test : 회원 이메일 변경 적용, 중복 이메일 예외 발생 테스트

--- 1Week_Mission/src/test/java/com/ll/ebook/service/member/MemberServiceTest.java
+++ 1Week_Mission/src/test/java/com/ll/ebook/service/member/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.ll.ebook.service.member;
 
+import com.ll.ebook.member.MemberRepository;
 import com.ll.ebook.member.exception.SignupEmailDuplicatedException;
 import com.ll.ebook.member.exception.SignupUsernameDuplicatedException;
 import com.ll.ebook.member.service.MemberService;
@@ -12,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -20,6 +22,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MemberServiceTest {
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("회원가입이 가능하다.")
@@ -48,6 +53,30 @@ public class MemberServiceTest {
 
         Exception exception = assertThrows(SignupEmailDuplicatedException.class, () -> {
             memberService.join("user1","1234", "user2@email.com", "");
+        });
+
+        String expectedMessage = "이미 사용중인 email 입니다.";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    @DisplayName("저장된 회원의 이메일을 변경한다.")
+    public void modifyEmail(){
+        memberService.join("user2","1234", "user2@email.com", "");
+
+        memberService.modify("user2", "user1@gmail.com", "");
+
+        assertThat(memberRepository.findByUsername("user2").get().getEmail()).isEqualTo("user1@gmail.com");
+    }
+
+    @Test
+    @DisplayName("저장된 회원 이메일 변경 후 중복된 이메일 예외 발생")
+    public void joinEmailDuplicatedExceptionAfterModify(){
+        memberService.join("user2","1234", "user2@email.com", "");
+        memberService.join("user1","1234", "user1@email.com", "");
+        Exception exception = assertThrows(SignupEmailDuplicatedException.class, () -> {
+            memberService.modify("user2","user1@email.com", "");
         });
 
         String expectedMessage = "이미 사용중인 email 입니다.";
